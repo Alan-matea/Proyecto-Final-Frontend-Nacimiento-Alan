e7cee41a7041d031d2234b2c08681392ad9338bc
Alan-matea <alan21gtaonline@gmail.com>
2020-10-02 17:43:23 -0300

    Agregue un AppBar y titulos para song y users

--- frontend-nacimiento-alan/src/App.js
+++ frontend-nacimiento-alan/src/App.js
@@ -1,61 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
-
-
-
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const obtenerUsers = async () => {
+    //Obtiene los usuarios desde el backend
+    const response = await fetch("http://localhost:4000/user/");
+    const data = await response.json();
+    setUsers(data);
+  };
+  useEffect(() => {
+    //Esto se ejecuta al renderizar la pagina por primera vez
+    obtenerUsers();
+  }, []);
 
-//   const [users, setUsers] = useState([]);
-//   const [songs, setSongs] = useState([]);
-
-//   const obtenerUsers = async () => {
-//     //Obtiene los usuarios desde el backend
-//     const response = await fetch("http://localhost:4000/user/");
-//     const data = await response.json();
-//     setUsers(data);
-//   };
-
-
-//   useEffect(() => {
-//     //Esto se ejecuta al renderizar la pagina por primera vez
-//     obtenerUsers();
-//   }, []);
-
-//   const handleClick = async (e, user) => {
-//     e.preventDefault();
-//     console.log("Se hizo click en el usuario", user);
-//     //Se le pega a la API que trae las canciones
-//     const response = await fetch("http://localhost:4000/user/");
-//     const data = await response.json();
-//     setSongs(data);
-//   };
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <p>Lista de usuarios</p>
-//         {users.length > 0 ? (
-//           users.map((user) => (
-//             <p className="User" key={user._id}>
-//               {user.name}
-//               <button onClick={(e) => handleClick(e, user)}>Canciones</button>
-//             </p>
-//           ))
-//         ) : (
-//           <p>Cargando...</p>
-//         )}
-//       </div>
-//       <div>
-//         <p>Lista de canciones</p>
-//         {songs.map((song) => (
-//           <p className="Song" key={song._id}>
-//             {song.name}
-//           </p>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  const handleClick = async (e, user) => {
+    e.preventDefault();
+    console.log("Se hizo click en el usuario", user);
+    //Se le pega a la API que trae las canciones
+    const response = await fetch("http://localhost:4000/user/");
+    const data = await response.json();
+    setSongs(data);
+  };
+  return (
+    <div className="App">
+      <div>
+        <p>Lista de usuarios</p>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <p className="User" key={user._id}>
+              {user.name}
+              <button onClick={(e) => handleClick(e, user)}>Canciones</button>
+            </p>
+          ))
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </div>
+      <div>
+        <p>Lista de canciones</p>
+        {songs.map((song) => (
+          <p className="Song" key={song._id}>
+            {song.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- frontend-nacimiento-alan/src/index.js
+++ frontend-nacimiento-alan/src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+import App from "./App";
 import TableUsers from "./TableUsers";
 import TableSongs from "./TableSongs";
+import AppBar from "./AppBar";
+import TitleUsers from "./TitleUsers";
+import TitleSongs from "./TitleSongs";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TableSongs />
+    <App />
+    <AppBar />
+    <TitleUsers />
     <TableUsers />
+    <TitleSongs />
+    <TableSongs />
   </React.StrictMode>,
   document.getElementById("root")
 );
